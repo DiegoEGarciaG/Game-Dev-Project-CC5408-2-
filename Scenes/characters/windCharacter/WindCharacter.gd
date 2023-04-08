d6028601037244eed3c1a433642a93866ef9acbf
SeaMira <sebastian.mira@ug.uchile.cl>
2023-04-08 14:36:30 -0400

    agregué proyectiles con area 2d, pero no tienen colision todavia. Tiene ahora el flip horizontal de la animación y creé unos layers para reconocer cuales son los objetos grabables, pusheables, personajes, etc. Esto último fue en lugar de hacerlo con groups

--- Scenes/characters/windCharacter/WindCharacter.gd
+++ Scenes/characters/windCharacter/WindCharacter.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal wind_cast(wind)
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
@@ -9,6 +10,19 @@ const ACCELERATION = 1000
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var animation_player = $AnimationPlayer
+@onready var canon = $Pivot/Canon
+@onready var pivot = $Pivot
+
+
+# direction
+var rot = 0
+
+# Shooting wind
+var wind_scene = preload("res://Scenes/characters/windCharacter/skill/wind.tscn")
+
+func _process(_delta):
+	if Input.is_action_just_pressed("cast"):
+		cast_wind()
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -21,14 +35,25 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+	var direction_x = Input.get_axis("ui_left", "ui_right")
+	var direction_y = Input.get_axis("ui_up", "ui_down")
+	if direction_x:
 		animation_player.play("walk")
-		var vector = Vector2(direction+1, 0)
-		$Sprite2D.flip_h = vector.min()
-		velocity.x = move_toward(velocity.x, direction*SPEED, ACCELERATION*delta)
+		pivot.scale.x = sign(direction_x)
+		velocity.x = move_toward(velocity.x, direction_x*SPEED, ACCELERATION*delta)
 	else:
 		animation_player.play("idle")
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	var vec = Vector2(direction_x, direction_y)
+	var angle = vec.angle()
+	canon.rotation = angle
 	move_and_slide()
+	
+	
+	
+	
+func cast_wind():
+	var w = wind_scene.instantiate()
+	w.global_position = canon.global_position
+	w.rotation = canon.rotation + PI/2
+	emit_signal("wind_cast", w)
