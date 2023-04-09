5de2554bb6fd99734835a05dc2d2806c8d5e68a6
letu76 <39562505+letu76@users.noreply.github.com>
2023-04-09 11:35:50 -0400

    connected level menu with test map

--- Scenes/menus/Levels_Menu/Levels_Menu.gd
+++ Scenes/menus/Levels_Menu/Levels_Menu.gd
@@ -17,3 +17,7 @@ func _on_back_button_pressed():
 
 func _on_exit_button_pressed():
 	get_tree().quit()
+
+
+func _on_button_pressed():
+	get_tree().change_scene_to_file("res://Scenes/maps/Testing Map.tscn")
