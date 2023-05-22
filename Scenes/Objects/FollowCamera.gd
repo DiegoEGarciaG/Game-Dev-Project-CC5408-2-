354556c20b37d2a64bb15e9c2ad884bfa1b29a69
DiegoEGarciaG <daigogar@gmail.com>
2023-05-21 20:31:06 -0400

    Se añade la cámara a lvl1, la cual está centrada entre ambos jugadores y provee limites al movimient, también se cambia el menu de pausa para estar centrado junto con la cámara.

---
+++ Scenes/Objects/FollowCamera.gd
@@ -0,0 +1,20 @@
+extends Camera2D
+
+@onready var follow_camera = $"."
+
+var nodes
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	nodes = get_tree().get_nodes_in_group("camera")
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	var new_position = Vector2.ZERO
+	var distance = Vector2.ZERO
+	new_position = nodes[0].global_position + nodes[1].global_position
+	distance = abs(nodes[0].global_position - nodes[1].global_position)
+	new_position /= nodes.size()
+	print(distance)
+	global_position = new_position
+	
