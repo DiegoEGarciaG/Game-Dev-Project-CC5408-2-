7089634ee3879cb6c8fb00428e5a4cf8508194c2
SeaMira <sebastian.mira@ug.uchile.cl>
2023-05-22 22:26:41 -0400

    pared activable

--- Scenes/maps/Testing Map.gd
+++ Scenes/maps/Testing Map.gd
@@ -10,3 +10,34 @@ func _ready():
 	
 func _on_player_wind_cast(wind):
 	winds.add_child(wind)
+	
+var box_speed = 3
+var move = false
+@onready var activation_wall = $ActivationWall
+var initial_position =Vector2(215, 225)
+var last_point = Vector2(215, 200)
+
+func _process(_delta):
+
+	if move:
+		while (activation_wall.transform.origin.y != 200):
+			activation_wall.velocity.y = -box_speed
+			activation_wall.move_and_slide()
+	else:
+		while (activation_wall.transform.origin.y != 225):
+			activation_wall.velocity.y = -box_speed
+			activation_wall.move_and_slide()
+
+
+
+func _on_activation_area_body_entered(body):
+	if body.is_in_group("player"):
+		print("alo")
+		move = true
+		
+
+
+func _on_activation_area_body_exited(body):
+	if body.is_in_group("player"):
+		print("hola")
+		move = false
