ca27938a917c2032b1522332d60b6acc4b73dfab
SeaMira <sebastian.mira@ug.uchile.cl>
2023-05-22 22:34:34 -0400

    terminada pared activable

--- Scenes/maps/Testing Map.gd
+++ Scenes/maps/Testing Map.gd
@@ -11,21 +11,22 @@ func _ready():
 func _on_player_wind_cast(wind):
 	winds.add_child(wind)
 	
-var box_speed = 3
+var box_speed = 100
 var move = false
 @onready var activation_wall = $ActivationWall
-var initial_position =Vector2(215, 225)
-var last_point = Vector2(215, 200)
+#var initial_position =Vector2(215, 225)
+#var last_point = Vector2(215, 215)
 
 func _process(_delta):
 
 	if move:
-		while (activation_wall.transform.origin.y != 200):
+		if (activation_wall.global_position.y >= 150):
+			print(activation_wall.global_position.y)
 			activation_wall.velocity.y = -box_speed
 			activation_wall.move_and_slide()
 	else:
-		while (activation_wall.transform.origin.y != 225):
-			activation_wall.velocity.y = -box_speed
+		if (activation_wall.global_position.y <= 225):
+			activation_wall.velocity.y = box_speed
 			activation_wall.move_and_slide()
 
 
