87bb8241ea85189000572aae25345124132e59d5
DiegoEGarciaG <daigogar@gmail.com>
2023-05-26 01:19:58 -0400

    Se añade condición de victoria en el juego

--- Scenes/levels/level1.gd
+++ Scenes/levels/level1.gd
@@ -8,6 +8,26 @@ func _ready():
 	preload("res://Scenes/characters/windCharacter/WindCharacter.tscn")
 	player.connect("wind_cast", _on_player_wind_cast)
 	
-	
+func _process(_delta):
+
+	if state1 and state2:
+		get_tree().quit()
+			
 func _on_player_wind_cast(wind):
 	winds.add_child(wind)
+
+var state1 = false
+var state2 = false
+var winCond1 = false
+var winCond2 = false
+func _on_baa_1_body_entered(body):
+	state1 = true
+
+func _on_baa_1_body_exited(body):
+	state1 = false
+
+func _on_baa_2_body_entered(body):
+	state2 = true
+
+func _on_baa_2_body_exited(body):
+	state2 = false
--- Scenes/maps/Testing Map.gd
+++ Scenes/maps/Testing Map.gd
@@ -33,12 +33,10 @@ func _process(_delta):
 
 func _on_activation_area_body_entered(body):
 	if body.is_in_group("player"):
-		print("alo")
 		move = true
 		
 
 
 func _on_activation_area_body_exited(body):
 	if body.is_in_group("player"):
-		print("hola")
 		move = false
