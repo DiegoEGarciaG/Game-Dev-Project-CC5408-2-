1c8a466672e5ca806c357767c05da9e76a6af352
Diego <diego.garcia.g@ug.uchile.cl>
2023-06-10 11:53:38 -0400

    Sprite para HookThing, trampoline añadidos y aplicados.

--- Scenes/Objects/HookThing.gd
+++ Scenes/Objects/HookThing.gd
@@ -5,6 +5,8 @@ extends StaticBody2D
 # Called when the node enters the scene tree for the first time.
 func collision_deac():
 	collision_shape_2d.disabled = true
+	visible = false
 	
 func collision_acti():
 	collision_shape_2d.disabled = false
+	visible = true
