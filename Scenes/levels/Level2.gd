8689c50a252721b2dae9fda759ce27c60325398e
DiegoEGarciaG <daigogar@gmail.com>
2023-07-03 00:20:05 -0400

    Se termina el lvl 2 mecánicamente, faltan detalles visuales y efectos de sonido

--- Scenes/levels/Level2.gd
+++ Scenes/levels/Level2.gd
@@ -7,41 +7,63 @@ extends Node2D
 #Objetos a desactivar
 @onready var primera_plat = $PhysicObjs/PrimeraPlat
 @onready var segunda_plat = $PhysicObjs/SegundaPlat
+@onready var tercera_plat = $PhysicObjs/TerceraPlat
 
 func _ready():
 	preload("res://Scenes/characters/windCharacter/WindCharacter.tscn")
 	player.connect("wind_cast", _on_player_wind_cast)
 	#Limites de la camara
 	#Hacer invisibles e intocables ciertos elementos
-	
-func _process(_delta):
-	print(plat1)
+	tercera_plat.call_deferred("collision_deac")
 
-var plat1 = false
+var platAira = false
+var platGrappel = false
 var deac1 = false
 var deac2 = false
-var winCond = false
+var winCond1 = false
+var winCond2 = false
 
 func _on_player_wind_cast(wind):
 	winds.add_child(wind)
 
-func _on_area_1_body_entered(body):
-	if body.is_in_group("player"):
-		if plat1:
-			if !deac1:
-				primera_plat.collision_deac()
-				deac1 = true
-		plat1 = true
-
 func _on_area_2_body_entered(body):
-	if body.is_in_group("player"):
+	if body.is_in_group("Aira"):
 		if !deac2:
-			segunda_plat.collision_deac()
+			segunda_plat.call_deferred("collision_deac")
 			deac2 = true
 
+func _on_grappels_area_2_body_entered(body):
+	if body.is_in_group("Grappel"):
+		tercera_plat.call_deferred("collision_acti")
 
-func _on_win_body_entered(body):
-	if body.is_in_group("player"):
-		if winCond:
+
+func _on_grappels_area_2_body_exited(body):
+	if body.is_in_group("Grappel"):
+		tercera_plat.call_deferred("collision_deac")
+
+
+func _on_airas_area_1_body_entered(body):
+	if body.is_in_group("Aira"):
+		platGrappel = true
+		if platAira && platGrappel:
+			primera_plat.call_deferred("collision_deac")
+
+
+func _on_grappels_area_1_body_entered(body):
+	if body.is_in_group("Grappel"):
+		platAira = true
+		if platAira && platGrappel:
+			primera_plat.call_deferred("collision_deac")
+
+
+func _on_airas_win_body_entered(body):
+	if body.is_in_group("Aira"):
+		winCond1 = true
+		if winCond2:
 			get_tree().change_scene_to_file("res://Scenes/menus/menu_background.tscn")
-		winCond = true
+
+func _on_grappels_win_body_entered(body):
+	if body.is_in_group("Grappel"):
+		winCond2 = true
+		if winCond1:
+			get_tree().change_scene_to_file("res://Scenes/menus/menu_background.tscn")
