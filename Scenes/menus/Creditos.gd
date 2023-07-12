b71ed76a28ddfe96dbc068f1b85f48167ab80b95
letu76 <39562505+letu76@users.noreply.github.com>
2023-07-12 18:17:48 -0400

    creditos

---
+++ Scenes/menus/Creditos.gd
@@ -0,0 +1,19 @@
+extends PanelContainer
+
+@onready var exit = $CanvasLayer/MarginContainer/PanelContainer/MarginContainer/VBoxContainer/Button
+@onready var hover_button_sound = $HoverButtonSound
+@onready var pressed_button_sound = $PressedButtonSound
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	exit.pressed.connect(_on_exit_pressed)
+	exit.mouse_entered.connect(_on_button_mouse_entered)
+	exit.pressed.connect(_on_button_mouse_entered)
+
+func _on_exit_pressed():
+	get_tree().change_scene_to_file("res://Scenes/menus/menu_background.tscn")
+	
+func _on_button_mouse_entered():
+	hover_button_sound.play(0.0)
+
+func _on_button_pressed():
+	pressed_button_sound.play(0.0)
